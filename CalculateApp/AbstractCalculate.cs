e61b396a363e852a8b1946916e3d8ec4ff2895bb
dingfeng <1041460265@qq.com>
2017-07-09 20:35:06 +0800

    first commit

---
+++ CalculateApp/AbstractCalculate.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateApp
+{
+    public abstract class AbstractCalculate : ILocalCalculateService
+    {
+        public const int LIGHT_SPEED = 299792458;  //光速
+        protected int count;
+        protected List<double> gridX;
+        protected List<double> gridY;
+        protected List<double> gridZ;
+        protected int _count;
+        protected Dictionary<Tuple<int, int, int>, Complex> pfMap = new Dictionary<Tuple<int, int, int>, Complex>();
+        protected Dictionary<Tuple<int, int, int>, double> absValueMap = new Dictionary<Tuple<int, int, int>, double>();
+        protected bool first = true;
+        /**
+         * 对一条数据进行计算
+         */
+        public void calculate(double antX, double antY, double Z, int freq, double measuredPhase)
+        {
+            count++;
+        }
+
+        /**
+         * 清空
+         */
+        public void clear()
+        {
+            this._count = 0;
+            this.pfMap.Clear();
+            this.absValueMap.Clear();
+            first = true;
+        }
+
+        /**
+         *初始化，三维坐标系上分配网格
+         */
+        public void init(ConfParam confParam)
+        {
+            clear();
+            int xCount = Convert.ToInt32((confParam.xEnd - confParam.xStart) / confParam.xInterval) + 1;
+            int yCount = Convert.ToInt32((confParam.yEnd - confParam.yStart) / confParam.yInterval) + 1;
+            int zCount = Convert.ToInt32((confParam.zEnd - confParam.zStart) / confParam.zInterval) + 1;
+            gridX = new List<double>();
+            fillList(gridX, confParam.xStart, confParam.xInterval, xCount);
+            gridY = new List<double>();
+            fillList(gridY, confParam.yStart, confParam.yInterval, yCount);
+            gridZ = new List<double>();
+            fillList(gridZ, confParam.zStart, confParam.zInterval, zCount);
+            for (int k = 0; k < zCount; k++)
+            {
+                for (int i = 0; i < yCount; i++)
+                {
+                    for (int j = 0; j < xCount; j++)
+                    {
+                        Tuple<int, int, int> tuple = new Tuple<int, int, int>(k,i,j);
+                        pfMap.Add(tuple,0);
+                        absValueMap.Add(tuple, 0);
+                    }
+                }
+            }
+        }
+
+        private void fillList(List<double> list, double start, double interval, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(start + i * interval);
+            }
+        }
+    }
+
+}
---
+++ CalculateApp/OriginSAR.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.ServiceModel;
+namespace CalculateApp
+{
+
+    public class OriginSAR : AbstractCalculate, ILocalCalculateService
+    {
+        /**
+         * 覆盖父类方法
+         */
+        private double predictedX;
+        private double predictedY;
+        private double predictedZ;
+        private const double THRESHOLD = 0;
+        private const int LEAST_COUNT = 100;
+        public new void calculate(double antX, double antY, double antZ, int freq, double measuredPhase)
+        {
+            //Console.WriteLine("antX: "+antX+" antY: "+antY+" antZ: "+antZ+" freq: "+freq+" measuredPhase: "+measuredPhase);
+            base.calculate(antX, antY, antZ, freq, measuredPhase);
+            double gridValueMax = double.MinValue;
+            double waveLength = LIGHT_SPEED / Convert.ToDouble(freq);
+            double distance = 0;
+            double idealPhase = 0;
+            double deltaPhase = 0;
+            double sum = 0;
+            Tuple<int, int, int>[] tuples = this.pfMap.Keys.ToArray<Tuple<int, int, int>>();
+            foreach (Tuple<int,int,int> tuple in tuples)
+            {
+                int k = tuple.Item1;
+                int i = tuple.Item2;
+                int j = tuple.Item3;
+                distance = Math.Sqrt(Math.Pow(antX - gridX[j], 2) + Math.Pow(antY - gridY[i], 2) + Math.Pow(antZ - gridZ[k], 2));
+                idealPhase = (4 * Math.PI * distance / waveLength) % (2 * Math.PI);
+                deltaPhase = (idealPhase - measuredPhase) % (2 * Math.PI);
+                Complex value = this.pfMap[tuple] + Complex.Exp(Complex.ImaginaryOne * deltaPhase);
+                pfMap[tuple] = value;
+                double sarAbsValue = Complex.Abs(value);
+                this.absValueMap[tuple] = sarAbsValue;
+                sum += sarAbsValue;
+                if (sarAbsValue > gridValueMax)
+                {
+                    gridValueMax = sarAbsValue;
+                    predictedX = gridX[j];
+                    predictedY = gridY[i];
+                    predictedZ = gridZ[k];
+                }
+            }
+            Dictionary<Tuple<int, int, int>, double> newAbsValueMap = new Dictionary<Tuple<int, int, int>, double>();
+            foreach (KeyValuePair<Tuple<int, int, int>, double> kvp in this.absValueMap)
+            {
+                Tuple<int, int, int> tuple = kvp.Key;
+                double proportion = kvp.Value / sum;
+                if (proportion > THRESHOLD || count < LEAST_COUNT)
+                {
+                    newAbsValueMap.Add(tuple,kvp.Value);
+                }
+                else if (proportion <= THRESHOLD)
+                {
+                    this.pfMap.Remove(tuple);
+                }
+            }
+            this.absValueMap = newAbsValueMap;
+            Console.WriteLine("predicted X: "+predictedX+" Y: "+predictedY+" Z: "+predictedZ);
+        }
+    }
+}
